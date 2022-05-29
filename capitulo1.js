6335a466d658d9b05be7b3f9467a93c18161f174
QuantummFiree <arteaga980910@gmail.com>
2022-05-28 21:48:49 -0500

    nuevo commit

--- capitulo1.js
+++ capitulo1.js
@@ -20,20 +20,31 @@ cofla = prompt("Ingrese su dinero cofla: ");
 function venderHelado(cliente, nCliente) {
     if(cliente >= poteCuartoKg){
         alert(`${nCliente} compro un pote 1/4 por ${poteCuartoKg}`);
+        cliente -= poteCuartoKg;
     }else if(cliente >= heladoConfites){
         alert(`${nCliente} compro un helado confites por ${heladoConfites}`);
+        cliente -= heladoConfites;
     }else if(cliente >= heladoHelardo){
         alert(`${nCliente} compro un helado helardo por ${heladoHelardo}`);
+        cliente -= heladoHelardo;
     }else if(cliente >= heladoHeladovich){
         alert(`${nCliente} compro un helado heladovich por ${heladoHeladovich}`);
+        cliente -= heladoHeladovich;
     }else if(cliente >= heladoHeladix){
         alert(`${nCliente} compro un helado heladix por ${heladoHeladix}`);
+        cliente -= heladoHeladix;
     }else if(cliente >= palitoHeladoC){
         alert(`${nCliente} compro un palito crema por ${palitoHeladoC}`);
+        cliente -= palitoHeladoC;
     }else if(cliente >= palitoHeladoA){
         alert(`${nCliente} compro un palito agua por ${palitoHeladoA}`);
+        cliente -= palitoHeladoA;
     }else{
         alert(`no cuenta con el dinero suficiente`);
+    }
+
+    if(nCliente == "Cofla"){
+        alert(`A Cofla le sobran ${cliente}`);
     }
 }
 venderHelado(roberto, nRoberto);
