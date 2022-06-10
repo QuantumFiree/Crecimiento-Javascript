b77e846af7c132b977cb1fabe061dc39a5b0a332
QuantummFiree <arteaga980910@gmail.com>
2022-06-10 01:26:11 -0500

    nuevo commit

---
+++ capitulo5.js
@@ -0,0 +1,16 @@
+// ejemplo de una promesa
+// datos = ["hola", "a", "todos"];
+
+// let a = () => {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             return resolve(datos);         
+//         }, 2000);
+//     })
+// };
+
+// a().then((resolve) => {
+//     return console.log(resolve);
+// });
+
+let container = document.querySelector(".Contenedor");
